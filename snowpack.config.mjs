f0742d1e979d0f5d2c2894d01f1e096e84a3f644
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-01-20 14:38:17 -0800

    Fix build settings etc

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -3,6 +3,10 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
+  root: "src",
+  buildOptions: {
+    out: "build"
+  },
   optimize: {
     bundle: true,
     minify: true
