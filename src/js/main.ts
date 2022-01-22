d4ed65ad42223fa01acd805199f715dc15ebf165
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-01-22 11:11:42 -0800

    Center design

--- src/js/main.ts
+++ src/js/main.ts
@@ -6,8 +6,8 @@ window.onresize = () => {
   canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
 };
 
-canvas.style.width = "100%"; // Note you must post fix the unit type %,px,em
-canvas.style.height = "400px";
+// canvas.style.width = "100%"; // Note you must post fix the unit type %,px,em
+// canvas.style.height = "400px";
 canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
 
 new ParticleRenderer(canvas).setup();
