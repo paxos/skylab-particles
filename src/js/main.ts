1fa80048cb813079b8e736eb0e707460a1cf146a
Patrick Dinger <121539+paxos@users.noreply.github.com>
2022-01-30 11:42:20 -0800

    Cleanup

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,13 +2,7 @@ import { ParticleRenderer } from "./ParticleRenderer";
 
 let canvas = document.getElementById("canvas") as any;
 
-window.onresize = () => {
-  // canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
-};
-
-// canvas.style.width = "100%"; // Note you must post fix the unit type %,px,em
-// canvas.style.height = "400px";
-// canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
+window.onresize = () => {};
 
 let maxSize = Math.max(window.innerWidth, window.innerHeight) * 1;
 
